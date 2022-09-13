a2a024a0cdb0ae89c9e947b1788bdfa7c3829fc9
slashpage15github <107419267+slashpage15github@users.noreply.github.com>
2022-09-13 12:21:49 -0500

    primero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rocketsapp';
+  data:any[]=[];
+  constructor(private http:HttpClient){}
+
+  ngOnInit() {
+    this.http.get("https://api.spacexdata.com/v4/rockets")
+    .subscribe((data:any) => {
+      this.data = data;
+    })
+  }
 }
